28a983f8dc5375c3f4849bbc514fbb5c74f5cfd3
John-Hershey <164906591+John-Hershey@users.noreply.github.com>
2024-04-23 15:12:29 -0400

    part 4 done- still needs part3 before use

--- part4.py
+++ part4.py
@@ -1,12 +1,15 @@
 from PIL import Image,ImageDraw
 #makes an image
-def make_img_arr(filename):
-    image = Image.open(filename)
-    imgArr = [image]
-    for i in range (1, 90):
-        imgArr.append(image.rotate(i*2))
+def make_img_arr():
+    imgArr = [Image.open("star2.png"),Image.open("zoomed.png"),
+              Image.open("cc90.png"),Image.open("upside_down.png"),
+              Image.open("reflected.png"),Image.open("reflect3.png"),
+              Image.open("sheared.png")]
     return imgArr
     
 #takes an image array and saves it as a GIF file
 def make_gif(imgArr):
     frame_one = imgArr[0]
+    frame_one.save("star_rotation1.gif", format="GIF", append_images=imgArr,
+               save_all=True, duration=100, loop=0)
+make_gif(make_img_arr)
